e07f872be6cea32dc8ffeb2116227ea371d25569
tallguy188 <cksghd4220@naver.com>
2022-07-26 17:08:27 +0900

    event, style 추가중

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const body = document.querySelector("body");
-
 let element = document.getElementById("info");
-
 const searchBtn = element.getElementsByClassName("search_btn");
-
 const sideBtn = element.getElementsByClassName("sidebar_btn");
+const sidemenu = document.getElementById("sidemenu");
+const contentarea = document.getElementById("contentArea");
 
-function handleClick(event) {
-  console.log(event);
-  loadPage();
+function handlePageClick(event) {
+  openNav();
 }
 
-function loadPage() {}
+function handleClick() {}
+
+function openNav() {
+  sidemenu.style.width = "300px";
+  contentarea.style.marginLeft = "300px";
+  body.style.background;
+}
+
+function closeNav() {
+  sidemenu.style.width = "0";
+  contentarea.style.marginLeft = "0";
+}
 
 if (element) {
   searchBtn[0].addEventListener("click", handleClick);
-
-  sideBtn[0].addEventListener("click", handleClick);
+  sideBtn[0].addEventListener("click", handlePageClick);
 }
