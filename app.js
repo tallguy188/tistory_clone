00ad57a7c7da29b7d2fa4b2087e0b4a9419f35da
tallguy188 <cksghd4220@naver.com>
2022-07-29 18:33:56 +0900

    making search event

--- app.js
+++ app.js
@@ -2,27 +2,30 @@ const body = document.querySelector("body");
 let element = document.getElementById("info");
 const searchBtn = element.getElementsByClassName("search_btn");
 const sideBtn = element.getElementsByClassName("sidebar_btn");
+const closeBtn = document.getElementsByClassName("closebtn");
 const sidemenu = document.getElementById("sidemenu");
-const contentarea = document.getElementById("contentArea");
 
 function handlePageClick(event) {
   openNav();
 }
 
-function handleClick() {}
+function handleClick(event) {
+  console.log(event);
+}
 
 function openNav() {
+  body.style.filter = "brightness(80%)";
   sidemenu.style.width = "300px";
-  contentarea.style.marginLeft = "300px";
-  body.style.background;
 }
 
 function closeNav() {
   sidemenu.style.width = "0";
-  contentarea.style.marginLeft = "0";
 }
+
+function openSearch() {}
 
 if (element) {
   searchBtn[0].addEventListener("click", handleClick);
-  sideBtn[0].addEventListener("click", handlePageClick);
+  sideBtn[0].addEventListener("click", openNav);
+  closeBtn[0].addEventListener("click", closeNav);
 }
