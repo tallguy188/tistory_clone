d6c500edb3988589267b8515300947d1ff71bb14
tallguy188 <cksghd4220@naver.com>
2022-07-30 16:15:23 +0900

    search box 수정중

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const body = document.querySelector("body");
+const header = document.querySelector("header");
 let element = document.getElementById("info");
 const searchBtn = element.getElementsByClassName("search_btn");
 const sideBtn = element.getElementsByClassName("sidebar_btn");
 const closeBtn = document.getElementsByClassName("closebtn");
 const sidemenu = document.getElementById("sidemenu");
-
+const searchbox = document.getElementById("searchbox");
+const searchclose = document.getElementsByClassName("searchclose");
 function handlePageClick(event) {
   openNav();
 }
@@ -22,10 +24,28 @@ function closeNav() {
   sidemenu.style.width = "0";
 }
 
-function openSearch() {}
+function openSearch() {
+  if (searchbox.style.display == "none") {
+    searchbox.style.display = "block";
+    header.style.display = "none";
+  } else {
+    searchbox.style.display = "none";
+  }
+}
+
+function closeSearch() {
+  if (searchbox.style.display == "block") {
+    searchbox.style.display = "none";
+    header.style.display = "block";
+  }
+}
 
 if (element) {
-  searchBtn[0].addEventListener("click", handleClick);
   sideBtn[0].addEventListener("click", openNav);
   closeBtn[0].addEventListener("click", closeNav);
+}
+
+if (searchbox) {
+  searchBtn[0].addEventListener("click", openSearch);
+  searchclose[0].addEventListener("click", closeSearch);
 }
